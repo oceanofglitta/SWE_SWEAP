85ae6af84ed78304548626fe203ec0cab7d59849
gahyun <sspkr1792@naver.com>
2021-11-13 02:59:42 +0900

    BuySell.js 매수/매도페이지+APP.js route 페이지 이동

--- sweap/src/App.js
+++ sweap/src/App.js
@@ -1,25 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BuySellPage from './components/BuySellPage';
+import MainPage from './components/MainPage';
+import SearchPage from './components/SearchPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<MainPage /> }/>
+            <Route path='/buysell'element={<BuySellPage /> }/>
+            <Route path='/search'element={<SearchPage /> }/>
+          </Routes>
+        </div>
+      </Router>
+      </>
+    );
+  }
 }
-
 export default App;
---
+++ sweap/src/components/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function SearchPage() { 
+    return ( 
+    <>  
+    <div>주식 정보 페이지</div>
+    </> 
+    ); 
+} 
+
+export default SearchPage;
