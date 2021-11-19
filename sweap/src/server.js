1985437ec3a56c73a193011db824cbab917db4e0
gogahyun <sspkr1792@naver.com>
2021-11-19 15:25:02 +0900

    sever에 올라간 db와 react 연동

--- sweap/src/server.js
+++ sweap/src/server.js
@@ -1,16 +1,16 @@
 const express = require("express"); 
 const app = express();
-const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
+const port = 8082; // react의 기본값은 3000이니까 3000이 아닌 아무 수
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql"); // mysql 모듈 사용
 
-var connection = mysql.createConnection({
-    host : "localhost",
-    user : "root", //mysql의 id
-    password : "0527", //mysql의 password
-    database : "sweapdb", //사용할 데이터베이스
-});
+ var connection = mysql.createConnection({
+     host : "18.118.194.10:3306",
+     user : "sweap", //mysql의 id
+     password : "sweappassword", //mysql의 password
+     database : "sweapdb", //사용할 데이터베이스
+ });
 
 connection.connect();
 
@@ -18,8 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-
-app.post("/TRANSACTION", (req,res)=>{
+app.post("/SQL", (req,res)=>{
     const post = req.body.query;
 
     connection.query(post,
@@ -28,13 +27,12 @@ app.post("/TRANSACTION", (req,res)=>{
             console.log("전송 실패");
         }else{
             console.log("전송 성공");
-            res.send(rows[0])
+            console.log(rows[0]);
+            res.send(rows[0]);
         };
-    });
-
-    
+    });    
 });
 
 app.listen(port, ()=>{
-    console.log(`Connect at http://localhost:${port}`);
+    console.log(`Connect at http://3.144.158.36:${port}`);
 })
